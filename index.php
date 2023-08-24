0d6097442cc5949b2e35799b22624a4bff652ad7
safwane-fdd <safwane_f@hotmail.com>
2023-08-24 18:50:29 +0200

    cours php

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php /*C'est là que se met du PHP */ 
+  echo "Ceci est du texte";
+  echo "Ceci est encore du <strong>texte</strong>";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Cours PHP - Débutant</title>
+</head>
+<body>
+
+  <h2>Cours PHP - Débutant</h2>
+
+  <p>Cette page contient du code PHP  
+    <?php /* Insérez du code PHP ici aussi */ ?>
+    Voici quelques petits tests :
+  </p>
+
+  <ul>
+    <li style="color: blue;">Text en bleu</li>
+    <li style="color: red;">Text en rouge</li>
+    <li style="color: green;">Text en vert</li>
+  </ul>
+
+  <?php /* Encore du PHP et toujours du PHP */  ?>
+
+</body>
+</html>
